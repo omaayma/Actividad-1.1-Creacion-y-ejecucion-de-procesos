89c51bccbaf0029b4a3fb93202061250131879df
omaayma <omaymazemmouri03@gmail.com>
2025-09-26 10:19:36 +0200

    Proograma principal

--- .idea/programa.java
+++ .idea/programa.java
@@ -3,8 +3,18 @@ public class programa{
         Scanner scanner = new Scanner(System.in); /*Usamos el scanner para leer la entrada del usuario*/
 
         String sistema = System.getProperty("os.name").toLowerCase();/*Nos va a devolver el nombre del sistema operativo*/
+        boolean esWin = false;
+        if(sistema.contains("win")) {
+            esWin = true; /*Con esto podremos saber si se trata del sistema operativo Windows*/
+        }
 
+        System.out.println("El sistema es:" + (esWin ? "Windows": "Linux"));
 
+        /*Ahora vamos a hacer un menú de las opciones que quiera elegir el usuario utilizando un bucle*/
+
+        while(true){
+            System.out.println("\n Escoga una ")
+        }
 
 
 
