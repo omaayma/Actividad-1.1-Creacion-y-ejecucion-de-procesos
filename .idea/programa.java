c87c6792a0efc38c68cfaedd56764d57418ca6e5
omaayma <omaymazemmouri03@gmail.com>
2025-09-26 09:58:41 +0200

    -

--- .idea/programa.java
+++ .idea/programa.java
@@ -1,3 +1,14 @@
 public class programa{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in); /*Usamos el scanner para leer la entrada del usuario*/
+
+        String sistema = System.getProperty("os.name").toLowerCase();/*Nos va a devolver el nombre del sistema operativo*/
+
+        boolean
+
+
+
+    }
+
 
 }
